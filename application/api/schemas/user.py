4cb5bedb5405cfe75c687c19824f5cd936c274b5
AntonioMont <garsiamarkes111@gmail.com>
2024-06-13 20:08:59 +0200

    13.06

---
+++ application/api/schemas/user.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field
+
+USERNAME_PATTERN = r"^[\p{L}\p{N}\.'-]+(?: [\p{L}\p{N}\.'-]+)*$"
+PASSWORD_PATTERN = r"^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\.,<>~!@#$%^&*()_+])[a-zA-Z0-9\.,<>~!@#$%^&*()_+]+$"
+
+
+class User(BaseModel):
+    username: str = Field(description="Username should be a string", min_length=5, max_length=250,
+                          pattern=USERNAME_PATTERN)
+    password: str = Field(description="Password should be a string", min_length=5, max_length=250,
+                          pattern=USERNAME_PATTERN)
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "username": "John Doe",
+                    "password": "apple007"
+                }
+            ]
+        }
+    }
