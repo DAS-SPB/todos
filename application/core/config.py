8a2f072e82883f51bc130c445d58405b60a4d256
AntonioMont <garsiamarkes111@gmail.com>
2024-07-21 18:26:26 +0200

    21.07

--- application/core/config.py
+++ application/core/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -8,8 +8,9 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     SALT: str
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(
+        env_file=".env"
+    )
 
 
 settings = Settings()
---
+++ tests/test_models.py
@@ -0,0 +1,73 @@
+import pytest
+
+from application.api.schemas.todo import TodoCreate
+from application.api.schemas.user import UserCreate
+
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+from application.core.config import settings
+
+uri = settings.DATABASE_URL
+
+client = MongoClient(uri)
+
+test_db = client['test_todo_app']
+test_collection_users = test_db['test_users']
+test_collection_todos = test_db['test_todos']
+
+
+@pytest.fixture(scope='function', autouse=True)
+def clean_db():
+    print("Cleaning up collections before test run...")
+    test_db.test_collection_todos.delete_many({})
+    test_db.test_collection_users.delete_many({})
+    yield
+
+
+def test_create_user():
+    user_data = {"username": "testuser", "password": "testpassword"}
+    user = UserCreate(**user_data)
+    assert user.username == "testuser"
+    assert user.password == "testpassword"
+
+
+def test_create_todo_all_fields():
+    todo_data = {"title": "Test todo", "description": "Test todo creation", "completed": True}
+    todo = TodoCreate(**todo_data)
+    assert todo.title == "Test todo"
+    assert todo.description == "Test todo creation"
+    assert todo.completed is True
+
+
+def test_create_todo_only_mandatory_fields():
+    todo_data = {"title": "Test todo", "description": "Test todo creation without 'completed' field"}
+    todo = TodoCreate(**todo_data)
+    assert todo.title == "Test todo"
+    assert todo.description == "Test todo creation without 'completed' field"
+    assert todo.completed is False
+
+
+def test_create_user_db():
+    user_data = {"username": "testuser", "password": "testpassword"}
+    result = test_db.test_collection_users.insert_one(user_data)
+    inserted_id = result.inserted_id
+    assert inserted_id is not None
+    print(f"Created user id: {inserted_id}")
+
+    test_db_user = test_db.test_collection_users.find_one({"_id": ObjectId(inserted_id)})
+    assert test_db_user['username'] == "testuser"
+    assert test_db_user['password'] == "testpassword"
+
+
+def test_create_todo_db():
+    todo_data = {"title": "Test todo", "description": "Test todo creation", "completed": True}
+    result = test_db.test_collection_todos.insert_one(todo_data)
+    inserted_id = result.inserted_id
+    assert inserted_id is not None
+    print(f"Created todo id: {inserted_id}")
+
+    test_db_todo = test_db.test_collection_todos.find_one({"_id": ObjectId(inserted_id)})
+    assert test_db_todo['title'] == "Test todo"
+    assert test_db_todo['description'] == "Test todo creation"
+    assert test_db_todo['completed'] is True
